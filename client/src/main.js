1a6b88b4487cf503785fa63ae02ca118521ce19d
yuhui_tuwu <tsuchiya.y.ag@gmail.com>
2017-11-09 17:40:25 +0900

    Install some packages and rewrite main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,27 +1,36 @@
 'use strict';
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  BrowserRouter,
-  HashRouter,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router/immutable'
+import configureStore, { history } from './store/configureStore';
+import Root from './containers/RootContainer.js';
+
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import AppContainer from './containers/AppContainer';
 
+export const store = configureStore();
+
+export default class App extends React.Component {
+  componentWillMount(){
+  }
+  render(){
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <RootContainer />
+        </ConnectedRouter>
+      </Provider>
+    );
+  }
+}
 
 const theme = createMuiTheme();
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
-    <div>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={AppContainer}/>
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <App />
   </MuiThemeProvider>
   ,
   document.getElementById("root")

3cbf5d99c79f1fd7d7955b58616e32ebd1e1ceed
toshi443 <toshi.443.aki@gmail.com>
2017-11-15 14:26:29 +0900

    botの登録actionの実装

--- client/src/components/garage/index.js
+++ client/src/components/garage/index.js
@@ -22,11 +22,17 @@ export default class Garage extends Component {
     this.state = {
       addNewModal: false,
       detailModal: false,
-      practiceModal: false
+      practiceModal: false,
+      botName: "",
+      gameId: 0,
+      url: "",
+      comment: ""
     };
 
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentWillMount(){
@@ -40,22 +46,45 @@ export default class Garage extends Component {
     this.setState({[modalName]: false});
   }
 
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let bot = {
+      name: this.state.botName,
+      gameId: this.state.gameId,
+      url: this.state.url,
+      comment: this.state.comment
+    };
+    this.props.onRegisterBot(bot);
+  }
+
   renderModals() {
     return(
       <div>
+        {/* botの登録modal */}
         <Modal
           isOpen={this.state.addNewModal}
           onRequestClose={() => this.closeModal("addNewModal")}
           title="Add your Bot"
           description="Botを登録しましょう"
         >
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <label htmlFor="botName">Bot name:</label>
-            <input className="u-full-width" type="text" placeholder="bot name" id="botName"/>
+            <input name="botName" value={this.state.name} onChange={this.handleChange} className="u-full-width" type="text" placeholder="bot name" id="botName"/>
             <label htmlFor="repositoryUrl">Repository URL:</label>
-            <input className="u-full-width" type="text" placeholder="repository url" id="repositoryUrl"/>
+            <input name="url" value={this.state.url} onChange={this.handleChange} className="u-full-width" type="text" placeholder="repository url" id="repositoryUrl"/>
+            <label htmlFor="gameId">Game name:</label>
+            <select name="gameId" value={this.state.gameId} onChange={this.handleChange}>
+              <option>Please select</option>
+              <option value="1">Reversi</option>
+            </select>
             <label htmlFor="botComment">Comment:</label>
-            <textarea className="u-full-width" placeholder="このBotの説明" id="botComment"></textarea>
+            <textarea name="comment" value={this.state.comment} onChange={this.handleChange} className="u-full-width" placeholder="このBotの説明" id="botComment"></textarea>
             <input className="button-primary" type="submit" value="Submit"/>
           </form>
         </Modal>

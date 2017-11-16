33dc0264f7c7edb4dd69c2d2df64c64697e3335f
toshi443 <toshi.443.aki@gmail.com>
2017-11-16 16:02:02 +0900

    gamesAction.jsの作成

---
+++ client/src/actions/gamesAction.js
@@ -0,0 +1,43 @@
+import api from '../api';
+import {mapGameIdToName} from '../utils';
+
+export const GAMES_RUN_MATCH = 'GAMES_RUN_MATCH';
+export const GAMES_RUN_MATCH_REQUEST = 'GAMES_RUN_MATCH_REQUEST';
+export const GAMES_RUN_MATCH_SUCCESS = 'GAMES_RUN_MATCH_SUCCESS';
+export const GAMES_RUN_MATCH_FAIL = 'GAMES_RUN_MATCH_FAIL';
+
+// run match
+export function runMatch(botId) {
+  return(dispatch, getState) => {
+    // dispatch(runMatchRequest(botId));
+    let params = new FormData();
+    params.append('botId', botId);
+    let url = "/api/v1/games/reversi/match";
+    api(getState).post(url, params).then( response => {
+      runMatchSuccess(response.data);
+    }).catch( error => {
+      runMatchFail(error);
+    })
+  }
+}
+
+function runMatchRequest(bot){
+  return {
+    type: GAMES_RUN_MATCH_REQUEST,
+    bot,
+  }
+}
+
+function runMatchSuccess(bot){
+  return {
+    type: GAMES_RUN_MATCH_SUCCESS,
+    bot,
+  }
+}
+
+function runMatchFail(error){
+  return {
+    type: GAMES_RUN_MATCH_FAIL,
+    error,
+  }
+}

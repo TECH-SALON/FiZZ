a3c157dd503e6f1fdd59d7a9d7e0833037f54e58
yuhui_tuwu <tsuchiya.y.ag@gmail.com>
2017-11-22 18:52:27 +0900

    Update lambda bots.py

--- sam/lambda/bots.py
+++ sam/lambda/bots.py
@@ -6,16 +6,20 @@ from pytz import timezone
 from datetime import datetime
 import uuid
 
-table_name = "Bots"
-if os.getenv("AWS_SAM_LOCAL"):
-    bots_table = boto3.resource(
-        'dynamodb',
-        endpoint_url="http://localhost:8000"
-    ).Table(table_name)
-else:
-    bots_table = boto3.resource('dynamodb').Table(table_name)
 
-class Bot():
+class Bot:
+    table_name = "Bots"
+
+    def __init__(self):
+        if os.getenv("AWS_SAM_LOCAL"):
+            self.bots_table = boto3.resource(
+                'dynamodb',
+                endpoint_url="http://localhost:8000"
+            ).Table(Bot.table_name)
+        else:
+            self.bots_table = boto3.resource('dynamodb').Table(Bot.table_name)
+
+
     def create_bot(self, accountId, gameId, name, isPrivate, repoUrl):
         utc = datetime.now(timezone('UTC'))
         item = {
@@ -32,16 +36,16 @@ class Bot():
             'createdAt': utc
         }
 
-        success, msg = validates(item)
+        success, attr = self.validates(item)
+
         # dynamodb
         if success:
-            resp = bots_table.put_item(
-                Item=item,
-            )
+            self.bots_table.put_item(Item=item);
             return (item, None)
-        return (None, msg)
 
-    def update_bot(self name=None, isPrivate=None, qualified=None, standBy=None, repoUrl=None, rank=None):
+        return (None, attr)
+
+    def update_bot(self, uuid, name=None, isPrivate=None, qualified=None, standBy=None, repoUrl=None, rank=None):
         utc = datetime.now(timezone('UTC'))
         item = {
             'name': name,
@@ -53,20 +57,51 @@ class Bot():
             'updatedAt': utc
         }
         # dynamodb
-        if validates(item):
-            break
-        return item
+        success, attr = self.validates(item):
 
-    def map_gameName_to_id(name):
-        return ''
+        if success:
+            updates = {}
+            for k, v in item:
+                if v is not None:
+                    updates[k] = { 'Action': 'PUT', 'Value': v}
+
+            resp = self.bots_table.update_item(
+                Key={'uuid': uuid },
+                AttributeUpdates=updates,
+                ReturnValues="ALL_NEW"
+            )
+            return (resp, None)
+        return (None, attr)
+
+
+
+    def query(self, table_name, key, value):
+        table = self.dynamodb.Table(table_name)
+        try:
+            res = table.query(
+                KeyConditionExpression = Key(key).eq(value)
+            )
+            return res
+        except Exception, e:
+            return e
+
+    def get(self, uuid):
+        self.query(Bot.table_name, 'uuid', uuid)
+
+    def scan(self):
+        # self.scan()
+
+    def transform_game_name2id(name):
+        res = self.query("Games", "name", name)
+        return res['uuid']
 
     def validates(item):
         # nameはアカウントごとにユニーク
         # それぞれのkeytypeをcheckする
         # if isPrivate is 1 then repoUrl is in FiZZ repo
         # gameId is valid
-        return (True, {})
-
+        error = {}
+        return (True, error)
 
 ####################### API #########################
 
@@ -99,11 +134,18 @@ def register_bot(event, context):
 
         gameName = event['pathParameters']['gameName']
 
-        if gameName is not 'reversi':
-            return {'statusCode': 400, 'body': 'Invalid game name.'}
+        bot = Bot()
+        resp, error = bot.create_bot(
+            accountId=body['accountId'],
+            gameId=bot.transform_game_name2id(gameName),
+            name=body['botName'],
+            isPrivate=body['isPrivate'],
+            repoUrl=body['repoUrl']
+        )
+        if error is None:
+            return {'statusCode': 201, 'body': str(resp)}
 
-
-        return {'statusCode': 400, 'body': 'This method is not supported.'}
+        return {'statusCode': 400, 'body': 'Request Failed'}
 
 #POST /api/v1/bots/:botId
 def stand_bot(event, context):
@@ -116,5 +158,14 @@ def stand_bot(event, context):
         except:
             return {'statusCode': 400, 'body': 'malformed json input'}
 
+    uuid = event['pathParameters']['botId']
 
-        return {'statusCode': 400, 'body': 'This method is not supported.'}
+    bot = Bot()
+    resp, error = bot.update_bot(
+        uuid, standBy=True
+    )
+
+    if error is None
+        return {'statusCode': 200, 'body': str(resp)}
+
+    return {'statusCode': 400, 'body': 'Request Failed'}

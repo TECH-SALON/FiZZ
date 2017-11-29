fcce509806e87926918d1a086d4cb5f1046f93d6
Yuhui Tuwu <yuhui.tuwu@gmail.com>
2017-11-29 10:32:58 +0900

    update fizz-aws for seed and list
    Update .env for lambda authentication
    
    add auth lambda login, sign up function
    
    Update auth and environments

--- Makefile
+++ Makefile
@@ -94,10 +94,10 @@ sam-local-generate-event:
 	$(SAM) local generate-event api > ./sam/lambda/functions/${FN}/event.json
 
 sam-local-invoke:
-	docker-compose run --rm sam local invoke ${AC} -e ./lambda/functions/${FN}/${EV}.json --docker-volume-basedir "${PWD}/sam/lambda/functions"
+	docker-compose run --rm sam local invoke ${AC} -t ${TEMP} -e ./lambda/functions/${FN}/${EV}.json --docker-volume-basedir "${PWD}/sam/lambda"
 
 sam-local-start-api:
-	docker-compose run --rm -p 3001:3000 sam local start-api -t ${TEMP} --docker-volume-basedir "${PWD}/sam/lambda/functions" --host 0.0.0.0
+	docker-compose run --rm -p 3001:3000 sam local start-api -t ${TEMP} --docker-volume-basedir "${PWD}/sam/lambda" --host 0.0.0.0
 
 sam-log:
 	docker-compose logs sam
--- sam/lambda/functions/auth/auth.py
+++ sam/lambda/functions/auth/auth.py
@@ -1,35 +1,140 @@
 import os
 import json
 import boto3
+import traceback
+import botocore
+import hmac
+import hashlib
+import base64
+
+
+class Cognito:
+    identity_pool_id = os.getenv("AWS_IDENTITY_POOL_ID", "")
+    user_pool_id = os.getenv("AWS_USER_POOL_ID", "")
+    client_id = os.getenv("AWS_CLIENT_ID", "")
+    client_secret = os.getenv("AWS_CLIENT_SECRET", "")
+    region = os.getenv("AWS_REGION", "us-east-1")
+
+    def __get_client(self):
+        return boto3.client('cognito-idp', 'us-east-1')
+
+    def __get_identity_client(self):
+        return boto3.client('cognito-identity')
+
+    def get_secret_hash(self, username):
+        message = username + self.client_id
+        dig = hmac.new(self.client_secret, msg=message.encode('UTF-8'),
+                       digestmod=hashlib.sha256).digest()
+        return base64.b64encode(dig).decode()
+
+    def sign_up(self, username, email, password):
+        return self.__get_client().sign_up(
+            ClientId=self.client_id,
+            Username=username,
+            Password=password,
+            UserAttributes=[
+                {
+                    'Name': 'email',
+                    'Value': email
+                }
+            ]
+        )
+
+    def login(self, username_or_alias, password):
+        return self.__get_client().admin_initiate_auth(
+            UserPoolId=self.user_pool_id,
+            ClientId=self.client_id,
+            AuthFlow='ADMIN_NO_SRP_AUTH',
+            AuthParameters={
+                'USERNAME': username_or_alias,
+                'PASSWORD': password,
+                'SECRET_HASH': self.get_secret_hash(username_or_alias)
+            }
+        )
 
 ####################### API #########################
 
-def get_sessions(event, context):
-    # Error Handling
-    # Parameters Check
-    # Return Response
-    return {'statusCode': 400, 'body': 'Request Failed'}
-
 def login(event, context):
     # Error Handling
-    # Parameters Check
-    # Return Response
+    try:
+        body = json.loads(event['body'])
+
+        # Parameters Check
+        username_or_alias = body['username_or_alias']
+        password = body['password']
+
+        print(f'Info: User Login Request {username}')
+        cognito = Cognito()
+        resp = cognito.login(username_or_alias, password)
+        auth = resp['AuthenticationResult']
+        ret = {
+            'challenge_name': resp['ChallengeName'],
+            'auth': {
+                'access_token': auth['AccessToken'],
+                'expires_in': auth["ExpiresIn"],
+                'token_type': auth['TokenType'],
+                'id_token': auth['IdToken'],
+                'refresh_token': auth['RefreshToken']
+            },
+            'session': resp['Session']
+        }
+
+        return {'statusCode': 200, 'body': str(ret)}
+    except:
+        traceback.print_exc()
+
     return {'statusCode': 400, 'body': 'Request Failed'}
 
-def sign_up(event, context):
-    # Error Handling
-    # Parameters Check
-    # Return Response
+
+def sign_up_with_provider(event, context):
+    try:
+        body = json.loads(event['body'])
+
+        provider = body['provider']
+        if provider == 'google':
+            pass
+        else:
+            return {'statusCode': 400, 'body': 'The Provider is not supported'}
+    except:
+        traceback.print_exc()
     return {'statusCode': 400, 'body': 'Request Failed'}
+
+
+def sign_up(event, context):
+    try:
+        body = json.loads(event['body'])
+
+        # Parameters Check
+        username = body['username']
+        email = body['email']
+        password = body['password']
+
+        print(f'Info: User SignUp Request {username}:{email}')
+        cognito = Cognito()
+        resp = cognito.sign_up(username, email, password)
+        ret = {
+            'confirmed': resp['UserConfirmed'],
+            'delivery_details': resp['CodeDeliveryDetails']
+        }
+
+        return {'statusCode': 201, 'body': str(ret)}
+    except:
+        traceback.print_exc()
+
+    return {'statusCode': 400, 'body': 'Request Failed'}
+
 
 def handler(event, context):
     try:
         if event['httpMethod'] == 'GET':
-            break
+            pass
         elif event['httpMethod'] == 'POST':
-            break
+            if event['path'] == '/api/v1/auth/signup':
+                return sign_up(event, context)
+            elif event['path'] == '/api/v1/auth/login':
+                return login(event, context)
         elif event['httpMethod'] == 'PUT':
-            break
+            pass
         return {'statusCode': 400, 'body': 'Request Failed'}
     except BaseException as e:
         print(e)

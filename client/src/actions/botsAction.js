efd43ef3dbe95bc854f8f273651b67bad5c81ee8
toshi443 <toshi.443.aki@gmail.com>
2017-11-30 11:20:10 +0900

    lambdaでpythonオブジェクトをjsonにencodeできるようにする

--- client/src/actions/botsAction.js
+++ client/src/actions/botsAction.js
@@ -69,7 +69,7 @@ export function getBots(refresh = false){
     }
 
     let url = `${endPoint()}/api/v1/bots`
-    console.log(url);
+
     api(getState).get(url).then( response => {
       let bots = response.data;
       dispatch(getBotsSuccess(bots));

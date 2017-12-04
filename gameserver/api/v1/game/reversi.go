9f328b9b5db19d28173319c9a0ada9f247be79ec
yuhui_tuwu <tsuchiya.y.ag@gmail.com>
2017-12-04 16:49:49 +0900

    comment out extra in gameserver

--- gameserver/api/v1/game/reversi.go
+++ gameserver/api/v1/game/reversi.go
@@ -29,33 +29,33 @@ func play(bots []models.Bot) *Response{
 
 
 
-type ImageName struct {
-	ImageName string
-}
-
-func runGM(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	switch r.Method {
-		case "GET":
-			log.Println(r)
-			fmt.Fprintf(w, "Please use POST method")
-		case "POST":
-			decoder := json.NewDecoder(r.Body)
-			var input ImageName
-			err := decoder.Decode(&input)
-			if err != nil {
-				log.Println(err)
-			}
-			defer r.Body.Close()
-			log.Println(input.ImageName)
-			var imageName = input.ImageName
-			countWin, err := ReversiGame.GameMaster(imageName)
-			log.Println(countWin)
-			if err != nil {
-				log.Println(err)
-			}
-			json.NewEncoder(w).Encode(countWin)
-			// w.Write([]byte(countWin))
-	}
-}
+// type ImageName struct {
+// 	ImageName string
+// }
+//
+// func runGM(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Access-Control-Allow-Origin", "*")
+// 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+// 	switch r.Method {
+// 		case "GET":
+// 			log.Println(r)
+// 			fmt.Fprintf(w, "Please use POST method")
+// 		case "POST":
+// 			decoder := json.NewDecoder(r.Body)
+// 			var input ImageName
+// 			err := decoder.Decode(&input)
+// 			if err != nil {
+// 				log.Println(err)
+// 			}
+// 			defer r.Body.Close()
+// 			log.Println(input.ImageName)
+// 			var imageName = input.ImageName
+// 			countWin, err := ReversiGame.GameMaster(imageName)
+// 			log.Println(countWin)
+// 			if err != nil {
+// 				log.Println(err)
+// 			}
+// 			json.NewEncoder(w).Encode(countWin)
+// 			// w.Write([]byte(countWin))
+// 	}
+// }

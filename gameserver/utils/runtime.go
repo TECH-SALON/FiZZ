81835fa2dcbc615f73eefed8379e074b17d6c785
yuhui_tuwu <tsuchiya.y.ag@gmail.com>
2017-12-27 10:19:51 +0900

    change bot model and runtime

--- gameserver/utils/runtime.go
+++ gameserver/utils/runtime.go
@@ -5,13 +5,18 @@ import (
   "path/filepath"
 )
 
-func GetRuntimeUrl(runtime string) string {
-  url := pwd() + "/" + runtime
-  return url
-}
-
-func GetRuntimeImageName(runtime string) string {
-  return "fz-"+runtime+"runtimei"
+func GetRuntimeImageName(runtime string) (string, error) {
+  err := fmt.Errorf("ERROR: bot runtime is invalid.")
+  switch runtime {
+  case "golang1.9":
+    return "fz-goruntimei", nil
+  case "python3.6":
+    return "fz-pyruntimei", nil
+  case "node9.3":
+    return "fz-jsruntimei", nil
+  default:
+    return nil, err
+  }
 }
 
 func pwd() string {
